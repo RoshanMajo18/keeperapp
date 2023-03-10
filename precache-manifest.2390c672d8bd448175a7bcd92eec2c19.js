7d262d86a541a9d3d581d314767c4aeaaa18bde3
RoshanMajo18 <roshanmajo18@gmail.com>
2023-03-10 21:28:36 +0530

    Updates

---
+++ precache-manifest.2390c672d8bd448175a7bcd92eec2c19.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "00fff6fc730d589b10cf",
+    "url": "/keeperapp/static/js/main.00fff6fc.chunk.js"
+  },
+  {
+    "revision": "032d731c673ad43df3c1",
+    "url": "/keeperapp/static/js/1.032d731c.chunk.js"
+  },
+  {
+    "revision": "b886af4f4d84e9847b03",
+    "url": "/keeperapp/static/js/runtime~main.b886af4f.js"
+  },
+  {
+    "revision": "e1a046425ddfbd9dc17a039659521073",
+    "url": "/keeperapp/index.html"
+  }
+];
